adc9522e43ccf979d8dfc7bfddd1def05eec2c9a
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2025-12-05 22:20:41 -0500

    work section created

--- src/app/sections/content/ContentSection.tsx
+++ src/app/sections/content/ContentSection.tsx
@@ -1,10 +1,12 @@
 'use client'
 import React from 'react'
 import styles from './ContentSection.module.scss'
-import { AboutSection } from '../about/AboutSection'
+import { AboutSection } from './about/AboutSection'
+import { WorkSection } from './work/WorkSection'
 
 export const ContentSection = () => {
   return <section className={`${styles['c-content-section']}`}>
-    <AboutSection/>
+    <AboutSection />
+    <WorkSection/>
   </section>
 }
